3f17192fe14e6f97179229312bcc30d09c645fe3
James Davis <developer@solvtopia.com>
2018-07-19 17:28:01 -0400

    Removed unnecessary project folders

--- EmailChecker/code/Objects.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports UtilityWizards.CommonCore.Shared.Common
-Imports UtilityWizards.CommonCore.Shared.Enums
-
-Public Structure RecordReturn
-    Public ID As Integer
-    Public [Type] As Enums.RecordTransactionType
-End Structure
--- EmailChecker/code/Processor.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports System.IO
-Imports System.Net
-Imports System.Text
-Imports System.Text.RegularExpressions
-
-Public Class Processor
-
-End Class
